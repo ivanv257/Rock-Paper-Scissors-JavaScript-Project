cc3af75b5ac050c608279c02b899f275cf9b1ddf
Ivan <Ivanv257@gmail.com>
2018-01-10 06:10:04 +0200

    script.js

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 // JavaScript Rock Paper Scissors Project
+
+/* set prompt for userChoice and Math.random
+for conditional statement for computer choice. */
+let userChoice = prompt("Rock, Paper or Scissors?");
+let computerChoice = Math.random();
+
+if (computerChoice < 0.34){
+  computerChoice = "rock";
+} else if (computerChoice <=0.67){
+  computerChoice = "paper";
+} else {
+  computerChoice = "scissors";
+}
