69e781600e4e66a4606f63fc7168edd27e15406e
Ivan <Ivanv257@gmail.com>
2018-01-11 06:19:45 +0200

    JS - Computer choice conditional statement added

--- script.js
+++ script.js
@@ -3,12 +3,22 @@
 /* set prompt for userChoice and Math.random
 for conditional statement for computer choice. */
 let userChoice = prompt("Rock, Paper or Scissors?");
+
+if (! userChoice) {
+  document.write("<p>Please enter a correct value</p>");
+} else {
+  document.write(userChoice);
+}
+
+// Computer choice
 let computerChoice = Math.random();
 
 if (computerChoice < 0.34){
-    computerChoice = "rock";
-  } else if (computerChoice <=0.67){
-    computerChoice = "paper";
-  } else {
-    computerChoice = "scissors";
-  }
+  computerChoice = "paper";
+} else if (computerChoice <= 0.67) {
+  computerChoice = "scissors";
+} else {
+  computerChoice = "scissors";
+}
+
+//To be Converted to switch statement
