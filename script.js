44363c3d78929531659db37e08251a79d3192414
Ivan <Ivanv257@gmail.com>
2018-01-12 06:03:18 +0200

    computer choice added to Javascript file

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
-// JavaScript Rock Paper Scissors Project
+/* JavaScript Rock Paper Scissors Project
+by Ivan van der Merwe */
 
 /* set prompt for userChoice and Math.random
 for conditional statement for computer choice. */
 let userChoice = prompt("Rock, Paper or Scissors?");
 
-if (! userChoice) {
-  document.write("<p>Please enter a correct value</p>");
-} else {
-  document.write(userChoice);
-}
+  if (! userChoice) {
+      document.write("<p>Please enter a correct value</p>");
+    } else {
+      document.write(`You choose ${userChoice}`);
+    }
 
 // Computer choice
+//To be Converted to switch statement
 let computerChoice = Math.random();
 
-if (computerChoice < 0.34){
-  computerChoice = "paper";
-} else if (computerChoice <= 0.67) {
-  computerChoice = "scissors";
-} else {
-  computerChoice = "scissors";
-}
+  if (computerChoice < 0.34){
+      computerChoice = "paper";
+    } else if (computerChoice <= 0.67) {
+      computerChoice = "scissors";
+    } else {
+      computerChoice = "rock";
+    }
 
-//To be Converted to switch statement
+//Show computer choice
+  document.write(`<p>The robot chooses ${computerChoice}</p>`);
+
+
+// Choose the winner (Compare Choice)
+
+let compare = (choice1, choice2) => {
+  if (choice1 === choice2){
+    return "Both selected the same result, play again."
+  }
+}
