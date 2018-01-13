a2b9e9aab6cccab09dabb18626a0c29121c3d1bf
Ivan <Ivanv257@gmail.com>
2018-01-13 13:46:49 +0200

    JavaScript code readability improved

--- script.js
+++ script.js
@@ -4,52 +4,52 @@ Developer: Ivan van der Merwe */
 // set prompt for userChoice
 let userChoice = prompt("Choose your Weapon: Rock, Paper or Scissors?");
 
-  if (! userChoice) {
-      document.write("<p>Please enter a correct value</p>");
-    } else {
-      document.write(`You choose ${userChoice}`);
-    }
+if (!userChoice) {
+  document.write("<p>Please enter a correct value</p>");
+} else {
+  document.write(`You choose ${userChoice}`);
+}
 
 // Computer choice
 let computerChoice = Math.random();
 
-  if (computerChoice < 0.34){
-      computerChoice = "paper";
-    } else if (computerChoice <= 0.67) {
-      computerChoice = "scissors";
-    } else {
-      computerChoice = "rock";
-    }
+if (computerChoice < 0.34) {
+  computerChoice = "paper";
+} else if (computerChoice <= 0.67) {
+  computerChoice = "scissors";
+} else {
+  computerChoice = "rock";
+}
 
 //Show computer choice
-  document.write(`<p>The Robot chooses ${computerChoice}</p>`);
-
+document.write(`<p>The Robot chooses ${computerChoice}</p>`);
 
 // Choose the winner (Compare Choice)
 
 let compare = (choice1, choice2) => {
-  if (choice1 === choice2){
-        return "Both selected the same result, play again.";
-    } if (choice1 === "rock" && choice2 === "scissors"){
-        return "You win!";
-    } else {
-        return "You lose, play again";
-    }
+  if (choice1 === choice2) {
+    return "Both selected the same result, play again.";
+  }
+  if (choice1 === "rock" && choice2 === "scissors") {
+    return "You win!";
+  } else {
+    return "You lose, play again";
+  }
 
   if (choice1 === "paper" && choice2 === "rock") {
-        return "You win!";
-    } else {
-        return "You lose, play again";
-    }
+    return "You win!";
+  } else {
+    return "You lose, play again";
+  }
 
   if (choice1 === "scissors" && choice2 === "rock") {
-          return "You lose, play again";
-      } else {
-          return "You win!";
-      }
- };
+    return "You lose, play again";
+  } else {
+    return "You win!";
+  }
+};
 // run the comparison function
-let results = compare(userChoice,computerChoice);
+let results = compare(userChoice, computerChoice);
 
 // print the results to the screen.
 document.write(`<br /> ${results}`);
