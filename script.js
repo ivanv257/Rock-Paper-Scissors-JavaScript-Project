4a455eafd7480e1faf4edff0d0b58dd705e30b35
ivanv257 <ivanv257@gmail.com>
2018-02-09 23:07:11 -0500

    new else if statement created

--- script.js
+++ script.js
@@ -1,47 +1,14 @@
 /* JavaScript Rock Paper Scissors Project
 Developer: Ivan van der Merwe */
 
-// set prompt for userChoice
-
-let userChoice = prompt("Choose your Weapon: Rock, Paper or Scissors?");
-if (!userChoice) {
-  document.write("<p>Please enter a correct value</p>");
+function computerChoice(){
+  let randomCount = Math.random();
+  
+  if (randomCount < 0.34){
+    return 'rock';
+} else if (randomCount > 0.34 && randomCount < 0.68){
+  return 'paper';
 } else {
-  document.write(`You choose ${userChoice}`);
+  return 'scissors';
 }
-// Computer choice
-let computerChoice = Math.random();
-if (computerChoice < 0.34) {
-  computerChoice = "paper";
-} else if (computerChoice <= 0.67) {
-  computerChoice = "scissors";
-} else {
-  computerChoice = "rock";
 }
-//Show computer choice
-document.write(`<p>The Robot chooses ${computerChoice}</p>`);
-// Choose the winner (Compare Choice) see if a switch statement would be applicable. 
-let compare = (choice1, choice2) => {
-  if (choice1 === choice2) {
-    return "Both selected the same result, play again.";
-  }
-  if (choice1 === "rock" && choice2 === "scissors") {
-    return "You win!";
-  } else {
-    return "You lose, play again";
-  }
-  if (choice1 === "paper" && choice2 === "rock") {
-    return "You win!";
-  } else {
-    return "You lose, play again";
-  }
-  if (choice1 === "scissors" && choice2 === "rock") {
-    return "You lose, play again";
-  } else {
-    return "You win!";
-  }
-};
-// run the comparison function
-let results = compare(userChoice, computerChoice);
-// print the results to the screen.
-document.write(`<br /> ${results}`);
