b7cf1b7bb091c1ade48706708b6dae2e24c24942
ivanv257 <ivanv257@gmail.com>
2018-02-10 00:28:11 -0500

    beautify Script

--- script.js
+++ script.js
@@ -8,26 +8,26 @@ const playerSelection = 'rock';
 
 let computerSelection = () => {
   let randomCount = Math.random();
-  
-  if (randomCount < 0.34){
+
+  if (randomCount < 0.34) {
     return 'rock';
-} else if (randomCount > 0.34 && randomCount < 0.68){
-  return 'paper';
-} else {
-  return 'scissors';
-}
+  } else if (randomCount > 0.34 && randomCount < 0.68) {
+    return 'paper';
+  } else {
+    return 'scissors';
+  }
 }
 
-function playRound(playerSelection, computerSelection){
-  if (playerSelection === computerSelection){
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
     return 'It is a tie';
-} else if (playerSelection === 'rock' && computerSelection === 'paper'){
+  } else if (playerSelection === 'rock' && computerSelection === 'paper') {
     return 'Computer wins!';
-} else if (playerSelection === 'rock' && computerSelection === 'scissors'){
+  } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
     return 'You win!';
-} else if (playerSelection === 'paper' && computerSelection === 'rock'){
+  } else if (playerSelection === 'paper' && computerSelection === 'rock') {
     return 'You win!';
-}
+  }
 }
 
 playRound(playerSelection, computerSelection());
